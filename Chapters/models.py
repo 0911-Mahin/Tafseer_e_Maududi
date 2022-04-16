d623c1054434d80da41070ef9bb8f8026ff70358
0911-Mahin <mahin.s0911@gmail.com>
2022-04-16 21:38:32 +0530

    Create Models For App Chapters

--- Chapters/models.py
+++ Chapters/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chapter(models.Model):
+    surah_number = models.IntegerField()
+    name = models.CharField(max_length=32)
+    verses = models.IntegerField()
+    revelation_place = models.CharField(max_length=16)
+    revelation_order = models.IntegerField()
+    translated_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'Surah {self.name}({self.translated_name}) was revealed in {self.revelation_place} with {self.verses} verses'
