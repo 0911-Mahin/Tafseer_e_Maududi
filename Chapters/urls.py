66fd80b4134abede342110f07c4aa3c02b670257
0911-Mahin <mahin.s0911@gmail.com>
2022-04-17 10:50:39 +0530

    Change Url Patterns

--- Chapters/urls.py
+++ Chapters/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'chapters', views.ChapterViewSet, basename='chapter')
+router.register(r'', views.ChapterViewSet, basename='chapter')
 
 urlpatterns = [
     path('', include(router.urls)),
