6cbb5e2872b4d9bbd1b8688adc8feec7f5b6880c
0911-Mahin <mahin.s0911@gmail.com>
2022-04-17 10:05:53 +0530

    Create API For Chapters

--- Chapters/views.py
+++ Chapters/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import ChapterSerializer
+from .models import Chapter
 
 # Create your views here.
+
+
+class ChapterViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Chapter.objects.all()
+    serializer_class = ChapterSerializer
