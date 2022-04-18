4a3b4394542b3add012e61762ca610b310d29694
0911-Mahin <mahin.s0911@gmail.com>
2022-04-18 16:08:15 +0530

    Setup Documentation For Chapters

--- API/urls.py
+++ API/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('chapters/', include('Chapters.urls'))
+    path('chapters/', include('Chapters.urls')),
+    path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs', SpectacularRedocView.as_view(
+        url_name='schema'), name='redoc'),
 ]
