22ba29746177e4d41a5bb326205b032b2fddebac
0911-Mahin <mahin.s0911@gmail.com>
2022-04-22 16:02:45 +0530

    Create/Configure App Hizb

--- API/urls.py
+++ API/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('hizb/', include('Hizb.urls')),
     path('docs/', TemplateView.as_view(
         template_name='docs.html',
     ), name='documentation'),
