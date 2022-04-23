5263c31b2994989c4955c47f86924c3c19e87169
0911-Mahin <mahin.s0911@gmail.com>
2022-04-23 21:20:23 +0530

    Complete Project Rub

--- Rub/urls.py
+++ Rub/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import RubViewSet
+
+router = DefaultRouter()
+router.register('', RubViewSet, basename='rub')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- Rub/views.py
+++ Rub/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Rub
+from .serializers import RubSerializer
+
+
+class RubViewSet(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'rub_number'
+    queryset = Rub.objects.all()
+    serializer_class = RubSerializer
