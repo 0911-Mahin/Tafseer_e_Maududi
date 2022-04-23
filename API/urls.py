e827a4c5089f9cbdd99f3a2fc12f62c46c5c2349
0911-Mahin <mahin.s0911@gmail.com>
2022-04-23 20:39:36 +0530

    Create/Configure App Rub

--- API/urls.py
+++ API/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('rub/', include('Ruc.urls')),
     path('docs/', TemplateView.as_view(
         template_name='docs.html',
     ), name='documentation'),
--- Tafseer_e_Maududi/settings.py
+++ Tafseer_e_Maududi/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'API',
+    'Rub',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
