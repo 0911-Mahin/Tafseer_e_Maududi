afe258506898cdc71d308dd827e80680972666b4
0911-Mahin <mahin.s0911@gmail.com>
2022-05-05 15:50:19 +0530

    Link App Verse With App Juz

--- Juz/serializers.py
+++ Juz/serializers.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from rest_framework import serializers
 from .models import Juz
 
@@ -5,7 +6,12 @@ from .models import Juz
 class JuzSerializer(serializers.ModelSerializer):
     detail = serializers.HyperlinkedIdentityField(
         view_name='juz-detail', lookup_field='juz_number')
+    verses = serializers.SerializerMethodField('get_verses_link')
+
+    def get_verses_link(self, juz):
+        request = self.context.get('request')
+        return request.build_absolute_uri(reverse('verse-by-juz', kwargs={'juz_number': juz.juz_number}))
 
     class Meta:
         model = Juz
-        fields = ['detail', 'id', 'juz_number', 'verse_count']
+        fields = ['detail', 'id', 'juz_number', 'verse_count', 'verses']
--- Verse/views.py
+++ Verse/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import status
 
 from Chapters.models import Chapter
+from Juz.models import Juz
 from .models import Verse
 from .serializers import VerseSerializer
 
@@ -12,10 +14,24 @@ class VerseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Verse.objects.all()
     serializer_class = VerseSerializer
 
-    @action(detail=True, methods=['get'])
+    @action(detail=False, methods=['get'], url_path=r'by_chapter/(?P<chapter_number>\d+)')
     def by_chapter(self, request, chapter_number):
-        chapter = Chapter.objects.get(pk=chapter_number)
-        page = self.paginate_queryset(chapter.verses.all())
-        serializer = VerseSerializer(
-            page, many=True, context={'request': request})
-        return Response(serializer.data)
+        try:
+            chapter = Chapter.objects.get(pk=chapter_number)
+            page = self.paginate_queryset(chapter.verses.all())
+            serializer = VerseSerializer(
+                page, many=True, context={'request': request})
+            return Response(serializer.data)
+        except Chapter.DoesNotExist:
+            return Response({'detail', 'Not Found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=False, methods=['get'], url_path=r'by_juz/(?P<juz_number>\d+)')
+    def by_juz(self, request, juz_number):
+        try:
+            juz = Juz.objects.get(pk=juz_number)
+            page = self.paginate_queryset(juz.verses.all())
+            serializer = VerseSerializer(
+                page, many=True, context={'request': request})
+            return Response(serializer.data)
+        except Juz.DoesNotExist:
+            return Response({'detail': 'Not Found.'}, status=status.HTTP_404_NOT_FOUND)
