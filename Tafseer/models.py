65101082861416fb99b8c7e742c5b76b7eee93e2
0911-Mahin <mahin.s0911@gmail.com>
2022-06-22 19:58:48 +0530

    Add Model Intro For App Tafseer

--- Tafseer/models.py
+++ Tafseer/models.py
@@ -6,3 +6,8 @@ from Verse.models import Verse
 class Tafseer(models.Model):
     text = models.FileField()
     verses = models.ManyToManyField(Verse, related_name='tafseer')
+
+
+class Intro(models.Model):
+    text = models.FileField()
+    tafseers = models.ManyToManyField(Tafseer, related_name='intro')
